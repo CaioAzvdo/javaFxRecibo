2dc616647bea5097024a9e49476651b51770f98f
CaioAzvdo <caioazevedodourado8@gmail.com>
2025-02-08 00:31:53 -0300

    Estilização dos inputs e botões do menu

--- src/main/java/org/plcdo/javafxrecibo/HelloApplication.java
+++ src/main/java/org/plcdo/javafxrecibo/HelloApplication.java
@@ -12,7 +12,7 @@ public class HelloApplication extends Application {
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
         Scene scene = new Scene(fxmlLoader.load(), 559, 420);
-        stage.setTitle("Impressor de Recibos_AlfaTeste");
+        stage.setTitle("Impressora de Recibos_AlfaTeste");
         stage.setScene(scene);
         stage.show();
     }
